48d7f8bdbe34c39b726880c3ff9ac0d5a0010337
Svenja Weiler <Svenja.Weiler@gmx.de>
2019-03-01 19:01:48 +0100

    ready for pull

--- client/src/components/pages/CodekunstDetail.jsx
+++ client/src/components/pages/CodekunstDetail.jsx
@@ -1,55 +1,64 @@
-import React, { Component } from 'react'
-import api from '../../api';
+import React, { Component } from "react";
+import api from "../../api";
 // import sketch from './sketch'
 // import P5Wrapper from './P5Wrapper';
 
 export default class CodekunstDetail extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      name: '',
-      code: '',
-      thumbnail: '',
+      name: "",
+      code: "",
+      thumbnail: "",
       codekunst: null
-    }
+    };
   }
   render() {
     if (!this.state.codekunst) {
-      return <div className="CodekunstDetail">Loading...</div>
+      return <div className="CodekunstDetail">Loading...</div>;
     }
     return (
       <div className="CodekunstDetail">
         <h1>CodekunstDetail</h1>
-        <strong>Projectcode</strong>: {this.state.codekunst.projectcode}<br/>
-        <img src={this.state.codekunst.thumbnail} alt="codekunstimage" className="thumbnail" />
+        <strong>Projectcode</strong>: {this.state.codekunst.projectcode}
+        <br />
+        <img
+          src={this.state.codekunst.thumbnail}
+          alt="codekunstimage"
+          className="thumbnail"
+        />
         {/* <P5Wrapper sketch={sketch} /> */}
-        <pre style={{textAlign: 'left', margin: 20}}>{this.state.code}</pre>
-        <div id="box" style={{border: '1px solid black'}}></div>
-
-        {this.state.codekunst.userarts.map((item, i) =>
-            <div key={i}>
-              <img src={item.pictureUrl} alt="userartimage" className="thumbnail" />
-              <br />
-              <strong>Creator: </strong><em>{item._user.username}</em>
-              <br />
-            </div>
-        )}
+        <pre style={{ textAlign: "left", margin: 20 }}>{this.state.code}</pre>
+        <div id="box" style={{ border: "1px solid black" }} />
+        {this.state.codekunst.userarts.map((item, i) => (
+          <div key={i}>
+            <img
+              src={item.pictureUrl}
+              alt="userartimage"
+              className="thumbnail"
+            />
+            <br />
+            <strong>Creator: </strong>
+            <em>{item._user.username}</em>
+            <br />
+          </div>
+        ))}
       </div>
-    )
+    );
   }
   componentDidMount() {
-    api.getCodekunstDetail(this.props.match.params.codekunstId)
+    api
+      .getCodekunstDetail(this.props.match.params.codekunstId)
       .then(codekunst => {
         this.setState({
           codekunst: codekunst,
           name: codekunst.name,
           code: codekunst.code,
           thumbnail: codekunst.thumbnail
-        })
+        });
         // eslint-disable-next-line
-        eval(codekunst.code)
-
+        eval(codekunst.code);
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   }
 }
