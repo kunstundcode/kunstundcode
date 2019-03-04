382691b9f3a85d8550b108edac0705d49cad5dc6
André Sebastian <a.sebastian@posteo.de>
2019-03-04 16:48:46 +0100

    prepare deploy

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 // Don't forget to set "MONGODB_URI" in ~/server/.env
-const uri = process.env.MONGODB_URI || `mongodb://localhost/kunstundcode`;
+const uri = process.env.MONGODB_URI 
+// || `mongodb://localhost/kunstundcode`;
 
 mongoose
   .connect(uri, { useNewUrlParser: true })
