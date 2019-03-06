4d4116d26ddf6501798f09942c1c81aba91a0518
André Sebastian <a.sebastian@posteo.de>
2019-03-06 10:25:01 +0100

    rename class to className

--- client/src/components/pages/ErrorPage404.jsx
+++ client/src/components/pages/ErrorPage404.jsx
@@ -5,8 +5,8 @@ export default class ErrorPage404 extends Component {
     render() {
         return (
           <div id="notfound">
-            <div class="notfound">
-              <div class="notfound-404">
+            <div className="notfound">
+              <div className="notfound-404">
                 <h3>Oops! Page not found</h3>
                 <h1><span>4</span><span>0</span><span>4</span></h1>
               </div>
